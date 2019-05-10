d4c3a2ffda7ff683c5417ec96f4ce3708f23c165
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-05-10 11:19:20 -0600

    added front end and back end files

---
+++ owl-app/src/StreamContainer/StreamContainer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class StreamContainer extends Component {
+    constructor(){
+        super();
+        this.state = {
+            stream : []
+        }
+    }
+    render() {
+        
+    }
+
+
+}
+
+
+
+export default StreamContainer;
