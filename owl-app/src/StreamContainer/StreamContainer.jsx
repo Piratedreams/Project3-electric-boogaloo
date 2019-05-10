b58a5e4895b2395d40df4d3d0e73d07964ec19fb
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-05-10 14:36:08 -0600

    stream container might be done.

--- owl-app/src/StreamContainer/StreamContainer.jsx
+++ owl-app/src/StreamContainer/StreamContainer.jsx
@@ -1,46 +1,42 @@
 import React, { Component } from 'react';
 
-class StreamContainer extends Component {
-    constructor(){
-        super();
-        this.state = {
-           stats: []
-        }
-    }
-    componentDidMount(){
-        this.searchStreams();
-    }
-     searchStreams = async () => {
-            try {
-                const response = await fetch('https://api.twitch.tv/helix/streams', {
-                    credentials: 'include'
-                });
-                if (response.status !== 200) {
-                    throw Error(response.statusText);
-                }
-                const streamParsed = await response.json();
-                this.setState({
-                    stream: streamParsed.data
-                })
 
-            } catch (err) {
-                console.log(err);
-            }
-        }
-    render(){
-        return(
-        <div id="twitch-embed"></div>
-            <script src="https://embed.twitch.tv/embed/v1.js"></script>
-            <script type="text/javascript">
-        new Twitch.Embed('twitch-embed', {
-                width: 854,
-                height: 480,
-                channel: "overwatchleague"
-          });
-            </script>
+
+const EMBED_URL = 'https://embed.twitch.tv/embed/v1.js';
+
+class StreamContainer extends Component {
+    componentDidMount() {
+        let embed;
+        const script = document.createElement('script');
+        this.setState(
+            'src',
+            EMBED_URL
+        );
+        this.addEventListener('load', () => {
+            embed = new window.Twitch.Embed(this.props.targetID, { ...this.props });
+        });
+        document.body.appendChild(script);
+    }
+
+    render() {
+
+        return (
+            <div>
+                StreamContainer {this.props.targetID} {this.props.width} {this.props.height}
+                <div id={this.props.targetID}>test</div>
+            </div>
         )
     }
 }
+
+StreamContainer.defaultProps = {
+    targetID: 'twitch-embed',
+    width: '940',
+    height: '480',
+    channel: 'overwatchleague'
+}
+
+
 
 
 export default StreamContainer;
